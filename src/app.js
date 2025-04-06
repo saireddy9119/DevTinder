38ffd2c737aa68d467d6e5d62bca45c7aad594e3
SaiReddy <sainarasimha@deskfactors.com>
2025-04-07 00:15:11 +0530

    add mongoose dependency and implement MongoDB connection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
+const connectDB = require('./config/database')
 
-app.listen(3000, () => {
-    console.log('server started on port 3000')
+connectDB().then(() => {
+    console.log("MongoDB connected successfully")
+    app.listen(3000, () => {
+        console.log('server started on port 3000')
+    })
+}).catch((err) => {
+    console.error("MongoDB connection failed", err.message)
 })
