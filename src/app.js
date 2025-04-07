343f7d842270e0b91c259d63984d34e03b6f6579
SaiReddy <sainarasimha@deskfactors.com>
2025-04-07 17:01:25 +0530

    implement user signup endpoint and create User model

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const app = express()
 const connectDB = require('./config/database')
+const User = require('./models/user')
+app.use(express.json())
 
+app.post("/signup", async (req, res) => {
+    // const userObj = {
+    //     firstName: "Sai",
+    //     lastName: "Reddy",
+    //     emailId: "sai@gmail.com",
+    //     password: "123456",
+    //     age: 22,
+    //     gender: "Male"
+    // }
+    //Creating a new instance of the User model
+    const user = new User(req.body)
+    try {
+        await user.save()
+        return res.status(200).json({ message: "User created successfully" })
+    } catch (err) {
+        return res.status(500).json({ message: "Error creating user", error: err.message })
+    }
+
+})
 connectDB().then(() => {
     console.log("MongoDB connected successfully")
     app.listen(3000, () => {
